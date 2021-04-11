1ae0609963081a65e2e7e55a3e7db1062af6e5a4
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-04-10 22:26:19 -0300

    asking user for number of rows

--- exercise1/resolution.c
+++ exercise1/resolution.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define BASE_LEN 50
+
+void getUserIntInput(char *str, int *reference) {
+	char input[BASE_LEN] = "";
+	char *endptr = NULL;
+	printf("%s\n", str);
+    fgets(input, BASE_LEN, stdin);
+    *reference = (int) strtol(input, &endptr, 10);
+	printf("\n");
+}
 
 int main() {
-   return 0;
+    int number_of_rows = 0;
+    getUserIntInput("Number of rows in the square matrix:", &number_of_rows);
+    return 0;
 }
